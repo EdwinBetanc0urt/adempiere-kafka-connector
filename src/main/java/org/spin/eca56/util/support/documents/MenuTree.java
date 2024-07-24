80e7a558610c525e359c835b5580dbcb454cce65
yamelsenih <ysenih@erpya.com>
2024-07-24 17:47:46 -0400

    Change load nodes from tree

--- src/main/java/org/spin/eca56/util/support/documents/MenuTree.java
+++ src/main/java/org/spin/eca56/util/support/documents/MenuTree.java
@@ -18,14 +18,14 @@
 package org.spin.eca56.util.support.documents;
 
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.MTree;
-import org.compiere.model.MTreeNode;
 import org.compiere.model.PO;
+import org.compiere.util.DB;
 import org.spin.eca56.util.support.DictionaryDocument;
 
 /**
@@ -42,59 +42,78 @@ public class MenuTree extends DictionaryDocument {
 		return KEY;
 	}
 	
-	/**
-	 * Add children to menu
-	 * @param context
-	 * @param builder
-	 * @param node
-	 */
-	private void addChildren(MTreeNode node, Map<String, Object> parent) {
-		Enumeration<?> childrens = node.children();
-		List<Map<String, Object>> children = new ArrayList<>();
-		while (childrens.hasMoreElements()) {
-			MTreeNode childNode = (MTreeNode)childrens.nextElement();
-			Map<String, Object> child = convertNode(childNode);
-			addChildren(childNode, child);
-			children.add(child);
-		}
-		parent.put("children", children);
-	}
-	
-	private Map<String, Object> convertNode(MTreeNode node) {
+	private Map<String, Object> convertNode(TreeNodeReference node) {
 		Map<String, Object> detail = new HashMap<>();
-		detail.put("node_id", node.getNode_ID());
-		detail.put("parent_id", node.getParent_ID());
-		detail.put("sequence", Integer.parseInt(node.getSeqNo()));
+		detail.put("node_id", node.getNodeId());
+		detail.put("parent_id", node.getParentId());
+		detail.put("sequence", node.getSequence());
 		return detail;
 	}
 	
 	@Override
 	public DictionaryDocument withEntity(PO entity) {
 		MTree tree = (MTree) entity;
-		MTreeNode rootNode = tree.getRoot();
-		return withNode(tree, rootNode);
+		return withNode(tree);
 	}
 	
-	public MenuTree withNode(MTree tree, MTreeNode node) {
-		if(node == null) {
-			return this;
-		}
-		Enumeration<?> childrens = node.children();
-		Map<String, Object> documentDetail = convertNode(node);
+	private List<TreeNodeReference> getChildren(int treeId, int parentId) {
+		String tableName = MTree.getNodeTableName(MTree.TREETYPE_Menu);
+		final String sql = "SELECT tn.Node_ID, tn.SeqNo "
+				+ "FROM " + tableName + " tn "
+				+ "WHERE tn.AD_Tree_ID = ? "
+				+ "AND tn.Parent_ID = ?";
+		List<Object> parameters = new ArrayList<Object>();
+		parameters.add(treeId);
+		parameters.add(parentId);
+		List<TreeNodeReference> nodeIds = new ArrayList<TreeNodeReference>();
+		DB.runResultSet(null, sql, parameters, resulset -> {
+			while (resulset.next()) {
+				nodeIds.add(TreeNodeReference.newInstance()
+						.withNodeId(resulset.getInt("Node_ID"))
+						.withParentId(parentId)
+						.withSequence(resulset.getInt("SeqNo")));
+			}
+		}).onFailure(throwable -> {
+			throw new AdempiereException(throwable);
+		});
+		return nodeIds;
+	}
+	
+	
+	public MenuTree withNode(MTree tree) {
+		List<TreeNodeReference> children = getChildren(tree.getAD_Tree_ID(), 0);
+		Map<String, Object> documentDetail = convertNode(TreeNodeReference.newInstance());
 		documentDetail.put("internal_id", tree.getAD_Tree_ID());
 		documentDetail.put("id", tree.getUUID());
 		documentDetail.put("uuid", tree.getUUID());
-		List<Map<String, Object>> children = new ArrayList<>();
-		while (childrens.hasMoreElements()) {
-			MTreeNode childNode = (MTreeNode)childrens.nextElement();
-			Map<String, Object> child = convertNode(childNode);
+		List<Map<String, Object>> childrenAsMap = new ArrayList<>();
+		children.forEach(child -> {
+			Map<String, Object> nodeAsMap = convertNode(child);
 			//	Explode child
-			addChildren(childNode, child);
-			children.add(child);
-		}
-		documentDetail.put("children", children);
+			addChildren(tree.getAD_Tree_ID(), child, nodeAsMap);
+			childrenAsMap.add(nodeAsMap);
+		});
+		documentDetail.put("children", childrenAsMap);
 		putDocument(documentDetail);
 		return this;
+	}
+	
+	/**
+	 * Add children to menu
+	 * @param context
+	 * @param builder
+	 * @param node
+	 */
+	private void addChildren(int treeId, TreeNodeReference node, Map<String, Object> parent) {
+		List<TreeNodeReference> children = getChildren(treeId, node.getNodeId());
+		List<Map<String, Object>> childrenAsMap = new ArrayList<>();
+		children.forEach(child -> {
+			Map<String, Object> nodeAsMap = convertNode(child);
+			//	Explode child
+			addChildren(treeId, child, nodeAsMap);
+			childrenAsMap.add(nodeAsMap);
+		});
+		parent.put("children", childrenAsMap);
 	}
 	
 	private MenuTree() {
