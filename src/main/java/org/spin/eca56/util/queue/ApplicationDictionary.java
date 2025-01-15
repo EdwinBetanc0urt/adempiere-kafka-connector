ede4cbed0bfa6ef90185c85f8cb778780546182c
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2025-01-15 12:04:24 -0400

    fix: Infinity loop when `Node_ID` equal `0`. (#64)
    * fix: Infinity loop when `Node_ID` equal `0`.
    
    * change with column I.
    
    * fix comments.

--- src/main/java/org/spin/eca56/util/queue/ApplicationDictionary.java
+++ src/main/java/org/spin/eca56/util/queue/ApplicationDictionary.java
@@ -64,6 +64,7 @@ public class ApplicationDictionary extends QueueManager implements IEngineDictio
 			queue.setProcessed(true);
 			queue.saveEx();
 		} catch (Throwable e) {
+			// e.printStackTrace();
 			logger.warning(e.getLocalizedMessage());
 		}
 	}
@@ -72,7 +73,7 @@ public class ApplicationDictionary extends QueueManager implements IEngineDictio
 	public void process(int queueId) {
 		send(queueId);
 	}
-	
+
 	private String getDictionaryCode() {
 		// Only system
 		MClientInfo clientInfo = MClientInfo.get(getContext(), 0);
@@ -82,7 +83,7 @@ public class ApplicationDictionary extends QueueManager implements IEngineDictio
 		}
 		return code.toLowerCase();
 	}
-	
+
 	public void send(int queueId) {
 		PO entity = getEntity();
 		if(entity != null) {
@@ -92,6 +93,7 @@ public class ApplicationDictionary extends QueueManager implements IEngineDictio
 				if(documentByLanguage != null) {
 					sender.send(documentByLanguage, documentByLanguage.getChannel());
 				}
+				// TODO: Skip with `AD_Tree` and `AD_Role`
 				getLanguages().forEach(languageId -> {
 					MLanguage language = new MLanguage(getContext(), languageId, getTransactionName());
 					IGenericDictionaryDocument aloneDocument = getDocumentManager(entity, language.getAD_Language());
@@ -105,7 +107,7 @@ public class ApplicationDictionary extends QueueManager implements IEngineDictio
 			logger.fine("Queue Processed: " + queueId);
 		}
 	}
-	
+
 	private List<Integer> getLanguages() {
 		return new Query(getContext(), I_AD_Language.Table_Name, "(IsBaseLanguage = 'Y' OR IsSystemLanguage = 'Y')", getTransactionName())
 				.setOnlyActiveRecords(true)
